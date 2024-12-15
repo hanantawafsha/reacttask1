011571c97723f52b0fc1d8e1e718e50ba446ebfb
hanantawafsha <hananjtawafsha@gmail.com>
2024-12-15 13:57:31 +0200

    update product

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import Product from "./product";
+import Product from "./Product"; 
 import { use } from "react";
 
 function App() {
@@ -33,6 +33,8 @@ const [showDetails,setShowDetails] = useState(false);
       </button>
 
       {showDetails && <Product />}
+
+      < Product />
     </>
   );
 }
